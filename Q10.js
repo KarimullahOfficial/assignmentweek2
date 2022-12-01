e2b64d3559e4bf08a1e1f3f0cddbe01cfc4a0a9c
mullahbalti456@gmail.com <mullahbalti456@gmail.com>
2022-12-01 11:13:35 +0500

    Q10 completed

---
+++ Q10.js
@@ -0,0 +1,7 @@
+function replacedigit(input) {
+
+    return input.replace(/[0-9]/, '$');
+  }
+  console.log(replacedigit("karim1dabc"));
+  console.log(replacedigit("p3ython"));
+  console.log(replacedigit("ab1cabc"));
