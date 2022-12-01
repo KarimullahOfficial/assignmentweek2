3c91da72946c94e705bb08c8b00804c0a9c14576
mullahbalti456@gmail.com <mullahbalti456@gmail.com>
2022-12-01 15:36:09 +0500

    Q16Completed

--- Q16.js
+++ Q16.js
@@ -1,7 +1,3 @@
-let a = [1, 2, 5,6,7]
-
-var min=Math.min(...a) 
-var max=Math.max(...a)
-console.log("min",min);
-console.log("max",max);
- 
+function modifyArray(nums) {
+    return (nums || []).map(num => num * (num % 2 === 0 ? 2 : 3));
+}
