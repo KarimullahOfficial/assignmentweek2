3de6cdf1ec18d68d9f8dfc75ff5c147e7ba94348
mullahbalti456@gmail.com <mullahbalti456@gmail.com>
2022-12-01 10:50:20 +0500

    Q6 Completed

---
+++ Q6.js
@@ -0,0 +1,16 @@
+function arr (a1, a2) {
+    var temp = [];
+    a1 = a1.toString().split(',').map(Number);
+    a2 = a2.toString().split(',').map(Number);
+    
+    for (var i in a1) {
+    if(a2.indexOf(a1[i]) === -1) temp.push(a1[i]);
+    }
+    for(i in a2) {
+    if(a1.indexOf(a2[i]) === -1) temp.push(a2[i]);
+    }
+    return temp.sort((a,b) => a-b);
+    }
+    
+    console.log( arr([1, 2, 3], [100, 2, 1, 10]));
+    console.log(arr([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
